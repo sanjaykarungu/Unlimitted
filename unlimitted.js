5dafeee2f4780e52797ae7875c0a230f8de0a3f2
sanjaykarungu <sanjaykarungu4690@gmail.com>
2025-11-08 11:48:10 +0530

    first commit

---
+++ unlimitted.js
@@ -0,0 +1,11 @@
+var sidenav = document.querySelector(".side-navbar")
+sidenav.style.display = "block"
+
+
+function shownavbar(){
+    sidenav.style.left="0"
+}
+
+function closenavbar(){
+    sidenav.style.left="-60%";
+}
